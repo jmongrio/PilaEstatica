d1d90bc1579b6ba677785b07455528fe194af5ae
Jmongrio <jmongrio@hotmail.com>
2021-04-05 21:39:34 -0600

    Se agrega al proyecto

---
+++ src/pilaestatica/PilaEstatica.java
@@ -0,0 +1,18 @@
+package pilaestatica;
+
+/**
+
+ @author Jason
+ */
+public class PilaEstatica
+{
+
+    /**
+     @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
